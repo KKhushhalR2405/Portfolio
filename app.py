9722026aa0ed942f464501d423400983509f6762
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2020-10-10 23:50:22 +0530

    database support

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import *
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/portfolio'
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+
+
+
 
 @app.route("/")
 def index():
@@ -10,8 +22,20 @@ def index():
 def gallery():
 	return render_template("gallery.html")
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET','POST'])
 def contact():
+
+	if request.method=='POST':
+
+		name = request.form.get('contact_name')
+		email = request.form.get('contact_email')
+		msg = request.form.get('contact_message')
+
+		entry = Contact(name=name,email=email,msg=msg)
+
+		db.session.add(entry)
+		db.session.commit()
+
 	return render_template("contact.html")
 
 
